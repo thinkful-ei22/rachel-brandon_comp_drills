2454f42df2155a656c08b5c9c0326f172ddc6cf9
rpinto <rachel.lea.pinto@gmail.com>
2018-07-23 12:01:40 -0700

    address book challenge finished

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -2,27 +2,34 @@ import React from 'react';
 
 import Contact from './contact';
 
-
 export default class AddressBook extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lists: [
-        {
-          header: 'Address Book'
-        }
-      ]
+      contacts: [{
+        name: 'Alice Aardvark',
+        photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+        address: '1600 Pennsylvania Ave'
+      }, {
+        name: 'Bob Bear',
+        photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+        address: '725 5th Ave'
+      }, {
+        name: 'Carol Coyote',
+        photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+        address: '4 Pennsylvania Plaza'
+      }]
     };
   };
 
   render() {
-
+  
     return (
-      <h1>{header}</h1>
-      <ul className="">
-        {contacts}
-      </ul>
+      <div>
+        <h1>Address Book</h1>
+        <Contact contacts = {this.state.contacts} />
+      </div>
     );
   }
 };
--- src/components/contact.js
+++ src/components/contact.js
@@ -4,11 +4,11 @@ import './contact.css';
 
 export default function Contact(props) {
   const contacts = props.contacts.map((contact, index) => (
-    <li key={index}>
-      <div>{contact.name}</div>
-      <img src={contct.photo} />
-      <div>{contact.address}</div>
+    <li className="contact" key={index}>
+      <div className="contact-name">{contact.name}</div>
+      <img className="contact-photo" src={contact.photo} />
+      <div className="contact-address">{contact.address}</div>
     </li>
   ));
-
+return (<ul>{contacts}</ul>);
 }
